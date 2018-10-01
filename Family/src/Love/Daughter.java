a614f327e006db39c6d82b5b3192c83d82243c4a
abhishek <abhishek@192.168.1.8>
2018-10-01 15:46:05 +0530

    First Commit

---
+++ Family/src/Love/Daughter.java
@@ -0,0 +1,10 @@
+package Love;
+
+public class Daughter {
+
+	public static void main(String[] args) {
+		System.out.println("I am daughter");
+
+	}
+
+}
---
+++ Family/src/Love/Son.java
@@ -0,0 +1,9 @@
+package Love;
+
+public class Son {
+
+	public static void main(String[] args) {
+		System.out.println("I am son");
+	}
+
+}
